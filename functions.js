2ec7964d050bb2eb663b93e4425fd18636f0f20f
USER <user@USERs-MBP.home>
2024-04-07 23:23:27 +0100

    historico

--- functions.js
+++ functions.js
@@ -3,23 +3,37 @@ const buttons = document.querySelectorAll("button");
 const operators = ["+","-","x","/","%","="];
 let output = "";
 let result = "";
-
+let memory = []
+let memoryback = 1
 const calculate = (value) => {
     console.log(value);
 
     if(value === "=" && output !== "")
     {
         result = output.replace("x","*");
+        memory.push(result)
         output = eval(result.replace("%", "/100"));
+        memoryback=1
     }
     else if(value === "AC")
     {
         output = "";
+        memoryback=1
     }
     else if(value === "DEL")
     {
         output = output.toString().slice(0, -1);
-    }else{
+    }
+     else if(value == "hist"){
+        if (memoryback <= memory.length) {
+            output = memory[memory.length-memoryback]
+            memoryback += 1
+        }else {
+            alert("voce voltou ate a ultima opreção")
+        }
+        
+    }   
+    else{
         if(output === "" && operators.includes(value)) return;
 
         output += value;
